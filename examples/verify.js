3d677471ebde0c42d109aea8f839285bdd85446e
Stefaan Ponnet <stefaan@ponnet.com>
2018-05-15 20:43:18 +0200

    tidy up schema + update verify script

--- examples/verify.js
+++ examples/verify.js
@@ -1,5 +1,11 @@
-var Validator = require('jsonschema').Validator;
-var v = new Validator();
-var instance = require('./metadata-manifest.json');
-var schema = require('./metadata-schema-0-1-0.json');
-console.log(v.validate(instance, schema));
+const Validator = require('jsonschema').Validator;
+const instance = require('./metadata-manifest.json');
+const schema = require('../spec/metadata-spec.json');
+
+let v = new Validator();
+let validation = v.validate(instance, schema);
+if(validation.errors.length !== 0){
+	console.log(validation.errors);
+}else{
+	console.log('file is valid');
+}
